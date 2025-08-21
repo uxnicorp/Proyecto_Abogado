3a652440e16216d22d72656808e5c001d774daa6
Ana Paula Soler <anapaulasoler711@gmail.com>
2025-08-21 17:46:47 -0300

    base React + vite

---
+++ src/paginas/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="home">
+      <h1>Asesoría legal clara y cercana</h1>
+      <p>
+        Derecho Civil, Penal y Laboral. Turnos online y atención personalizada.
+      </p>
+      <a className="btn" href="/contacto">
+        Solicitar consulta
+      </a>
+    </section>
+  );
+}
+
+export default Home
