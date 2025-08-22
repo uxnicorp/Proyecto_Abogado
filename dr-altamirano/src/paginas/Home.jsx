c1d0c4924646298550d38148f8bb2c296c4b3547
Ana Paula Soler <anapaulasoler711@gmail.com>
2025-08-21 22:32:33 -0300

    creo pagina de servicios

--- dr-altamirano/src/paginas/Home.jsx
+++ dr-altamirano/src/paginas/Home.jsx
@@ -1,5 +1,7 @@
 import "./css/Home.css";
 import Testimonios from "../componentes-generales/Testimonios";
+import { Link } from "react-router-dom";
+
 
 const Home = () => {
   return (
@@ -9,7 +11,7 @@ const Home = () => {
           <p className="subtitle">Su Abogada de mayor confianza desde 2005</p>
           <h1 className="title">Servicios legales de primer nivel</h1>
           <div className="btn-wrapper">
-            <a href="/contacto" className="btn-home">
+            <a href="/servicios" className="btn-home">
               SERVICIOS
             </a>
           </div>
